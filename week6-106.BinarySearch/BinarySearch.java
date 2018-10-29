7295361c8a4cb6c95cdda5de881bb71690770ff4
Lucas Godina <37753274+lucasgodina@users.noreply.github.com>
2018-10-29 18:12:57 -0300

    Add files via upload

---
+++ week6-106.BinarySearch/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static boolean search(int[] array, int searchedValue) {
+        int beginning = 0;
+        int end = array.length - 1;
+        int middle = (beginning + end) / 2;
+
+        while (beginning <= end) {
+
+            // restrict the search area
+            if (array[middle] == searchedValue) {
+                return true;
+
+            } else if (array[middle] < searchedValue) {
+                beginning = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+
+            middle = (beginning + end) / 2;
+
+        }
+        return false;
+    }
+}
